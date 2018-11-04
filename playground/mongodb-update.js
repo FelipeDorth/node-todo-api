ffb4e8eb98fe4830af50235c962efa7350c6e610
felipedorth <felipedorth@hotmail.com>
2018-11-04 18:12:19 -0200

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Users').findOneAndUpdate({
+  //   _id: new ObjectID('5bdf17706104ad30d4df49ab')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5bdf17706104ad30d4df49ab')
+  }, {
+    $set: {
+      name: Felipe
+    }
+  }, {
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+
+
+  // db.close();
+});
